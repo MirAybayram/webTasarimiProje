ad033f15de008273aabf7941126175ec0f20890d
MirAybayram <melikemiraybyrm@gmail.com>
2023-10-19 03:27:07 +0300

    webTasarimiProje

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Adınızı Yazınız",
+                    minlength: "Adınız en az 2 karakterden oluşmalı"
+                },
+                subject: {
+                    required: "Konu giriniz",
+                    minlength: "Konunuz en az 4 karakterden oluşmalıdır"
+                },
+                number: {
+                    required: "Numara giriniz?",
+                    minlength: "Numaranız en az 5 karakterden oluşmalıdır"
+                },
+                email: {
+                    required: "e-postayok, mesaj yok"
+                },
+                message: {
+                    required: "um...evet, bu formu göndermek için bir şeyler yazmalısın.",
+                    minlength: "Hepsi bu mu? Gerçekten mi?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
